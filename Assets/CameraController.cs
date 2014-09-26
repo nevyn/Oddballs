053f049e7a2d23c156cee531294c90c4aae4a5d7
Joachim Bengtsson <joachimb@gmail.com>
2014-09-27 01:43:40 +0200

    Sphere collider cat

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -11,7 +11,7 @@ public class CameraController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		GameObject kittyBall = GameObject.Find("KittyBall");
-		transform.LookAt(kittyBall.transform.position + new Vector3(0, 3, 0));
+		transform.LookAt(kittyBall.transform.position + new Vector3(0, 2, 0));
 		Vector3 diff = kittyBall.transform.position - transform.position;
 		if(diff.magnitude > 10)
 			transform.position += diff.normalized * 0.05f;
