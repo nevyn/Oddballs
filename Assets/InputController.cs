3c54bb5a7bc9e9e866a220c8247c1257ac5ca411
Joachim Bengtsson <joachimb@gmail.com>
2014-09-26 20:55:20 +0200

    four balls

---
+++ Assets/InputController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputController : MonoBehaviour {
+
+	public string key = "a";
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(key)) {
+			rigidbody.AddForce(Vector3.up * 50, ForceMode.Force);
+
+		}
+	}
+}
