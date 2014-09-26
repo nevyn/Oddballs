5b37e508139c34d0d707e37109900b8a77c4f856
Joachim Bengtsson <joachimb@gmail.com>
2014-09-27 00:04:33 +0200

    Camera controls

---
+++ Assets/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+		GameObject kittyBall = GameObject.Find("KittyBall");
+		transform.LookAt(kittyBall.transform.position + new Vector3(0, 3, 0));
+		Vector3 diff = kittyBall.transform.position - transform.position;
+		if(diff.magnitude > 10)
+			transform.position += diff.normalized * 0.05f;
+
+		transform.position = new Vector3(transform.position.x, kittyBall.transform.position.y + 3, transform.position.z);
+	}
+}
