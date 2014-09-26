344d351a29e7f11211c539dcf67b635b81ed932d
inkku <iekman@77-80-130-206.event.dreamhack.se>
2014-09-26 22:48:44 +0200

    Rolling

--- Assets/InputController.cs
+++ Assets/InputController.cs
@@ -6,16 +6,40 @@ public class InputController : MonoBehaviour {
 	public string key = "a";
 	public int impulse_force = 10;
 
+	private Transform myCorner;
+	private bool onGround=false;
+
 	// Use this for initialization
 	void Start () {
+
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKeyDown(key)) {
-			rigidbody.AddForce(Vector3.up * impulse_force, ForceMode.Impulse);
-			
+
+				if (Input.GetKeyDown (key)) {
+						if (onGround) {
+								rigidbody.AddForce (Vector3.up * impulse_force, ForceMode.Impulse);
+						}
+				}
 		}
-	}
+
+	void OnCollisionStay(Collision col){
+		Debug.Log (col.gameObject.tag);
+
+				if (col.gameObject.tag == "Ground") {
+						onGround = true;
+			Debug.Log (onGround);
+				}
+		}
+	void OnCollisionExit(Collision col){
+				if (col.gameObject.tag == "Ground") {
+						onGround = false;
+			Debug.Log (onGround);
+				}
+
+			
+		}           
 }
+
