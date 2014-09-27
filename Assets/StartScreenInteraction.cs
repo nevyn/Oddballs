455260248731ce0e9f9db6dc49393fa565298b65
inkku <iekman@77-80-130-149.event.dreamhack.se>
2014-09-27 18:36:28 +0200

    Menu with sound

---
+++ Assets/StartScreenInteraction.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartScreenInteraction : MonoBehaviour {
+
+	public string key = "a";
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.anyKeyDown) {
+			//Input.GetButtonDown ("Jump " + key) || 
+			Debug.Log ("BUTTONPRESS");
+			Application.LoadLevel ("First"); 
+		}
+
+	}
+}
